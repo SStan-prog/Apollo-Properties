958dc19d120d06c5bd33c1ecc6d38c2020156472
Scott Stanfel <scottstanfel@Scotts-MacBook-Pro.local>
2023-01-26 11:46:17 +0900

    update faq

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,28 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
-import sanity from "astro-sanity";
+import sanity from 'astro-sanity';
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), solidJs(), react(), sanity({
-    projectId: "3ans7o2s",
-    dataset: "production"
-  })]
+  integrations: [
+    tailwind(),
+    image(),
+    react(),
+    sanity({
+      projectId: '3ans7o2s',
+      dataset: 'production',
+    }),
+  ],
 });
