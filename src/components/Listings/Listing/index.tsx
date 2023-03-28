339e59df86d3bdab95774591aa81696ca33a9914
Scott Stanfel <scottstanfel@Scotts-MacBook-Pro.local>
2023-03-29 08:11:32 +0900

    rearrange listing

--- src/components/Listings/Listing/index.tsx
+++ src/components/Listings/Listing/index.tsx
@@ -35,9 +35,10 @@ const index = ({ listings, activeListing }: ListingProps) => {
                     {' '}
                     ¥{listing.price}
                   </span>{' '}
-                  per month
+                  <span className=" whitespace-nowrap">per month</span>
                 </p>
               </div>
+
               <div className="flex flex-col w-full gap-2">
                 <p className=" text-grey-600 text-xl">
                   <strong className="text-blue-600">Available from:</strong>{' '}
@@ -47,17 +48,6 @@ const index = ({ listings, activeListing }: ListingProps) => {
                   <strong className="text-blue-600">Size:</strong>{' '}
                   {listing.size} m<sup>2</sup>
                 </p>
-                <div className="">
-                  <p className="text-blue-600 font-bold text-xl">
-                    About the property
-                  </p>
-                  <div className="text-grey-600 text-lg">
-                    <PortableText value={listing.description} />
-                  </div>
-                </div>
-              </div>
-              <div className="w-full mt-10">
-                <Slider images={listing.images} />
               </div>
               <a
                 href="/contact"
@@ -65,6 +55,23 @@ const index = ({ listings, activeListing }: ListingProps) => {
               >
                 Contact Us About This Listing
               </a>
+              <div className="w-full my-10">
+                <Slider images={listing.images} />
+              </div>
+              <div className="">
+                <p className="text-blue-600 font-bold text-xl">
+                  About the property
+                </p>
+                <div className="text-grey-600 text-lg">
+                  <PortableText value={listing.description} />
+                </div>
+                <a
+                  href="/contact"
+                  className="bg-blue-400 hover:bg-blue-600 text-white text-lg sm:text-2xl font-body py-2 px-4 inline-block font-bold rounded-md hover:shadow-none shadow-lg whitespace-nowrap transition-all mt-10"
+                >
+                  Contact Us About This Listing
+                </a>
+              </div>
             </div>
           </div>
         );
