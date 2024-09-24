705c3eb9f7b6a55d72de17f52fdf4b742780a488
Scott Stanfel <scottstanfel@gmail.com>
2024-09-24 13:28:36 +0900

    add back blog

--- src/components/Common/Navbar.svelte
+++ src/components/Common/Navbar.svelte
@@ -8,8 +8,8 @@
   };
 </script>
 
-<nav class=" mx-auto max-w-[1600px] relative ">
-  <div class=" px-5 sm:px-10   ">
+<nav class=" mx-auto max-w-[1600px] relative">
+  <div class=" px-5 sm:px-10">
     <div
       class="relative flex justify-between lg:grid lg:grid-cols-10 items-center"
     >
@@ -23,7 +23,7 @@
         </a>
       </div>
       <div class=" col-span-7 hidden lg:block">
-        <ul class=" justify-between items-center flex ">
+        <ul class=" justify-between items-center flex">
           <li>
             <a href="/fully-furnished-apartments" class=" text-lg">
               Fully Furnished Apartments
@@ -40,6 +40,9 @@
             <a href="/testimonials" class=" text-lg"> Testimonials </a>
           </li>
           <li>
+            <a href="/blog" class=" text-lg"> Blog </a>
+          </li>
+          <li>
             <a
               href="/contact"
               class="bg-blue-400 hover:bg-blue-600 text-white text-lg font-body py-2 px-4 font-bold rounded-md shadow-lg hover:shadow-none inline-block max-w-[min-content] whitespace-nowrap transition-all"
@@ -50,7 +53,7 @@
         </ul>
       </div>
       <button
-        class="flex flex-col gap-[9px]  lg:hidden px-2 "
+        class="flex flex-col gap-[9px] lg:hidden px-2"
         on:click={toggle}
         aria-label="Open mobile menu"
       >
